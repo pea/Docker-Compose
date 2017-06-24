1ee9c05a274145d5c8033eb497cd799a592e0e4e
Peter Bailey <prodigy.zero@gmail.com>
2017-06-24 22:17:49 +0100

    Added Themosis

---
+++ LAMP-Themosis/README.md
@@ -0,0 +1,18 @@
+# Themosis WordPress Framework
+
+LAMP server with Themosis Framework
+
+# Setup
+
+- Themosis latest
+- PHP 7
+- Apache 2
+- Mysql 5.7
+
+# Database
+
+Host: db:3306
+Password: wordpress
+Database Name: wordpress
+Username: wordpress
+Password: wordpress
---
+++ LAMP-Themosis/docker-entrypoint.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -euo pipefail
+
+if ! [ -e /var/www/html/htdocs/index.php ]; then
+	# Install Composer		
+	curl -sS https://getcomposer.org/installer | php
+	mv composer.phar /usr/local/bin/composer
+	alias composer='/usr/local/bin/composer'
+
+	# Install Themosis	
+	composer create-project themosis/themosis /var/www/html
+
+	# Configure environment settings
+	cat > /var/www/html/.env.local <<-'EOF'
+		DB_NAME = "wordpress"
+		DB_USER = "wordpress"
+		DB_PASSWORD = "wordpress"
+		DB_HOST = "db:3306"
+		WP_HOME = "http://localhost/htdocs"
+		WP_SITEURL = "http://localhost/htdocs/cms"
+	EOF
+
+	cat > /var/www/html/config/environment.php <<-'EOF'
+		<?php
+
+		/*----------------------------------------------------*/
+		// Define environment type
+		/*----------------------------------------------------*/
+		return [
+		    'local' => '*',
+		    'production' => 'INSERT-HOSTNAME'
+		];
+	EOF
+
+fi
+
+exec "$@"
